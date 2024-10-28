a89f600cd7606dc6cbcf916826dab9621d69d69b
yujin <yjkimliz03@gmail.com>
2024-10-28 10:56:54 +0900

    Initial commit.

---
+++ reactprj/src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+import Component1 from './Component1';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          {/* Edit <code>src/App.js</code> and save to reload. */}
+          20241028 react oss class
+        </p>
+        <Component1 />
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
