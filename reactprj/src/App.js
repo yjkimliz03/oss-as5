d8bcec4ed4503d0512bc048f577b1e1908c039ef
yujin <yjkimliz03@gmail.com>
2024-10-28 19:48:22 +0900

    test push

--- reactprj/src/App.js
+++ reactprj/src/App.js
@@ -13,9 +13,11 @@ function App() {
           {/* Edit <code>src/App.js</code> and save to reload. */}
           20241028 react oss class
         </p>
+        
         <Component1 />
         <Component2 />
         <Component3 />
+
         <a
           className="App-link"
           href="https://reactjs.org"
