22794fbbad746dcb2827a1063dc902f654c710c6
yujin <yjkimliz03@gmail.com>
2024-11-05 03:35:52 +0900

    Delete other folder, remain only React project.

--- reactprj/src/Component2.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react'
-
-export default class Component1 extends Component {
-  render() {
-    return (
-      <div>Component2 react!!!</div>
-    )
-  }
-}
--- reactprj/src/Component3.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react'
-
-export default class Component1 extends Component {
-  render() {
-    return (
-      <div>Component3 react!!!</div>
-    )
-  }
-}
