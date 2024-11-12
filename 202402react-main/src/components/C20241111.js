8e6ef0c94175feb4a5b325a0a22648c561e49beb
yujin <yjkimliz03@gmail.com>
2024-11-12 14:28:04 +0900

    Initial commit

---
+++ 202402react-main/src/components/C20241111.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function C20241111(props) {
+  return (
+    <>
+      <div>C20241111 : {props.name}!!22</div>
+      <div>obj.name = {props.obj.name}</div>
+      <div>obj.name1 = {props.obj.name1}</div>
+      <div>obj.number = {props.obj.number}</div>
+    </>
+  )
+}
---
+++ 241111_react/src/components/Componentc.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class Componentc extends Component {
+  render() {
+    return (
+      <div>Componentc : react!!!</div>
+    )
+  }
+}
